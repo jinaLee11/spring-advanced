094de805e28fd32724bc717368f1c56af14d8cd2
J <justonesip82@gmail.com>
2025-04-18 18:35:26 +0900

    docs: Lv2. N+1 문제
    - TodoRepository의 N+1 해결방법 분석 후 주석 추가

--- src/main/java/org/example/expert/domain/todo/repository/TodoRepository.java
+++ src/main/java/org/example/expert/domain/todo/repository/TodoRepository.java
@@ -11,6 +11,14 @@ import java.util.Optional;
 
 public interface TodoRepository extends JpaRepository<Todo, Long> {
 
+    /** Lv2. 분석
+     * Todo와 User는 다대일 관계로 매핑되어있는 상태고,
+     * Todo 전체 목록을 가져오기 위해서는 우선 Todo 목록을 조회하는 쿼리 1번 + Todo와 이어진 User도 가져오기 위해 N번의 쿼리 돌림
+     *  => N+1 문제 발생 (성능이 떨어진다고 한다)
+     * 해당 Repository에서는 fetch join을 사용해 N+1문제를 해결했다.
+     * fetch join은 join과 비슷하지만 연관된 엔티티나 컬렉션을 한 번의 쿼리로 가져올 수 있다!
+     */
+
     @Query("SELECT t FROM Todo t LEFT JOIN FETCH t.user u ORDER BY t.modifiedAt DESC")
     Page<Todo> findAllByOrderByModifiedAtDesc(Pageable pageable);
 
