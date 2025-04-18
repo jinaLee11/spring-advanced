e22e9e08c7fa9b64e311daa081c21cf0cfe63e74
J <justonesip82@gmail.com>
2025-04-18 21:00:22 +0900

    refactor : Lv 3-1. 테스트코드 연습-1
    - matches 메서드의 매개변수 위치 수정

--- src/test/java/org/example/expert/config/PasswordEncoderTest.java
+++ src/test/java/org/example/expert/config/PasswordEncoderTest.java
@@ -15,14 +15,16 @@ class PasswordEncoderTest {
 
     @Test
     void matches_메서드가_정상적으로_동작한다() {
-        // given
+        // given 준비
         String rawPassword = "testPassword";
         String encodedPassword = passwordEncoder.encode(rawPassword);
 
-        // when
-        boolean matches = passwordEncoder.matches(encodedPassword, rawPassword);
+        // when 실행
+        // 기존 코드 : boolean matches = passwordEncoder.matches(encodedPassword, rawPassword);
+        // 실행시 Expected :true, Actual :false => matches 매개변수의 순서를 잘못 넣었기 때문 !
+        boolean matches = passwordEncoder.matches(rawPassword, encodedPassword); // BUILD SUCCESSFUL in 3s
 
-        // then
+        // then 검증
         assertTrue(matches);
     }
 }
